bff0931731c851fbba6bbf3e5dbe8f6791584716
sandhya755 <yavvarisandhyakumari@gmail.com>
2024-07-23 12:45:15 +0530

    day1

---
+++ marks_average.py
@@ -0,0 +1,7 @@
+name=input("enter your name")
+m1=int(input("english marks:"))
+m2=int(intput("maths marks:"))
+m3=int(input("hindi marks:"))
+sum=m1+m2+m3
+average=sum//3
+print(f"Iam {name} I have scored {sum} marks in 3 subjects,the average of my score {avg}")
